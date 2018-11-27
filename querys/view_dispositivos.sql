72ee5427347e0e6c3884efc9a3b2676860dc465e
rodrigerar <rodrigerar@gmail.com>
2018-11-27 10:41:11 -0300

    agrega campo alarma en la view de dispositivo

--- querys/view_dispositivos.sql
+++ querys/view_dispositivos.sql
@@ -1,9 +1,13 @@
-SELECT 
-	dispositivo.nombre AS Reloj,
-    count(fichada.id_dispositivo) AS CantFichadas,
-    dispositivo.ultima_conexion AS UltConexion
-FROM
-	RRHH.dispositivo 
-INNER JOIN RRHH.fichada ON (dispositivo.id = fichada.id_dispositivo)
-GROUP BY dispositivo.nombre
-ORDER BY count(fichada.id_dispositivo) DESC;
+select
+    `RRHH`.`dispositivo`.`nombre` AS `Reloj`,
+    count(`RRHH`.`fichada`.`id_dispositivo`) AS `CantFichadas`,
+    `RRHH`.`dispositivo`.`ultima_conexion` AS `UltConexion`,
+    IF(DATEDIFF(`RRHH`.`dispositivo`.`ultima_conexion`, NOW())<-1, 1,0) AS `Alarma`
+from
+    (`RRHH`.`dispositivo`
+join `RRHH`.`fichada` on
+    ((`RRHH`.`dispositivo`.`id` = `RRHH`.`fichada`.`id_dispositivo`)))
+group by
+    `RRHH`.`dispositivo`.`nombre`
+order by
+    count(`RRHH`.`fichada`.`id_dispositivo`) desc;
